d31102f9faef8bc2a31648c2a33e1da1dbb236f3
quanru <quanruzhuoxiu@gmail.com>
2015-05-10 19:15:27 +0800

    修改格式

--- task/task0003/JS/index.js
+++ task/task0003/JS/index.js
@@ -1,22 +1,22 @@
-var newTaskNum = 1;//新建任务号
-var newClassNum = 1;//新建类别号
-var nowClass = "defaultClass";//当前操作的类别
-var nowTask = " ";//当前操作的任务
-var taskCount = 0;//总任务数
+var newTaskNum = 1,//新建任务号
+	newClassNum = 1,//新建类别号
+	nowClass = "defaultClass",//当前操作的类别
+	 nowTask = " ",//当前操作的任务
+	 taskCount = 0;//总任务数
 
 function todo() {//主程序
-    var selAllEle = document.getElementById("selAll");
-    var newClassEle = document.getElementById("newClass");
-    var newTaskEle = document.getElementById("newTask");
-    var selClass = document.getElementById("lists");
-    var selTask = document.getElementById("tasks");
-    var doneEle = document.getElementById("doneButton");
-    var editEle = document.getElementById("editButton");
-    var detailsEle = document.getElementById("taskDetail");
-    var allTasks = document.getElementById("allTasks");
-    var undoTasks = document.getElementById("undoTasks");
-    var doneTasks = document.getElementById("doneTasks");
-    var defaultClass = document.getElementById("defaultClass");
+    var selAllEle = $("selAll"), 	
+    		newClassEle = $("newClass"),
+ 		newTaskEle = $("newTask"),
+ 		selClass = $("lists"),
+ 		selTask = $("tasks"),
+ 		doneEle = $("doneButton"),
+ 		editEle = $("editButton"),
+ 		detailsEle = $("taskDetail"),
+ 		allTasks = $("allTasks"),
+ 		undoTasks = $("undoTasks"),
+ 		doneTasks = $("doneTasks"),
+ 		defaultClass = $("defaultClass");
     defaultClass.taskCount = 0;
     defaultClass.newClassName = "默认分类";
     selAllEle.onclick = selAllTask;
@@ -37,12 +37,12 @@ function todo() {//主程序
 function addNewClass(event) {//添加类别事件
     var newClassName = prompt("请输入添加分类的名称", "新建分类" + newClassNum);
     if (newClassName !== null) {
-        var listsEle = document.getElementById("lists");
-        var newClassLi = document.createElement("li");
-        var newLiImg = document.createElement("img");
-        var newClassTextNode = document.createTextNode(newClassName);
-        var newClassSpan = document.createElement("span");
-        var newSpanTextNode = document.createTextNode("(0)");
+        var listsEle = $("lists"),
+        	newClassLi = document.createElement("li"),
+        	newLiImg = document.createElement("img"),
+        	newClassTextNode = document.createTextNode(newClassName),
+        	newClassSpan = document.createElement("span"),
+        	newSpanTextNode = document.createTextNode("(0)");
         newClassLi.id = "newClass" + newClassNum;
         newClassLi.newClassName = newClassName;
         newClassLi.classNum = newClassNum;
@@ -63,12 +63,12 @@ function addNewClass(event) {//添加类别事件
 }
 
 function addNewTask(event) {//添加任务事件
-    var newTaskDiv = document.getElementById("newTaskEdit");
-    var newHeadDiv = document.getElementById("newTaskHead");
-    var newDateDiv = document.getElementById("newTaskDate");
-    var newConDiv = document.getElementById("newTaskCon");
-    var addBtn = document.getElementById("addBtn");
-    var canBtn = document.getElementById("canBtn");
+    var newTaskDiv = $("newTaskEdit"),
+    		newHeadDiv = $("newTaskHead"),
+    		newDateDiv = $("newTaskDate"),
+    		newConDiv = $("newTaskCon"),
+    		addBtn = $("addBtn"),
+    		canBtn = $("canBtn");
     newHeadDiv.style.color = "#DDD";
     newDateDiv.style.color = "#DDD";
     newConDiv.style.color = "#DDD";
@@ -90,14 +90,14 @@ function clearIt(event) {//点击之后，清空输入框事件
 }
 
 function clickAddTask(event) {//点击添加，提交任务之后，触发的事件
-    var newTaskDiv = document.getElementById("newTaskEdit");
-    var newHeadDiv = document.getElementById("newTaskHead");
-    var newDateDiv = document.getElementById("newTaskDate");
-    var newConDiv = document.getElementById("newTaskCon");
-    var tasksEle = document.getElementById("tasks");
-    var newTaskName = newHeadDiv.value;
-    var NewTaskDate = newDateDiv.value;
-    var NewTtaskDetail = newConDiv.value;
+    var newTaskDiv = $("newTaskEdit"),
+    		newHeadDiv = $("newTaskHead"),
+    		newDateDiv = $("newTaskDate"),
+    		newConDiv = $("newTaskCon"),
+    		tasksEle = $("tasks");
+    var newTaskName = newHeadDiv.value,
+    		NewTaskDate = newDateDiv.value,
+    		NewTtaskDetail = newConDiv.value;
     if (newTaskName.length > 5) {
         alert("请输入少于五个字符的标题！");
         return;
@@ -108,11 +108,11 @@ function clickAddTask(event) {//点击添加，提交任务之后，触发的事
         alert("请输入少于140个字符的任务描述！");
         return;
     }
-    var getNowClass = document.getElementById(nowClass);
-    var allTask = document.getElementById("selAll");
-    var newTaskLi = document.createElement("li");
-    var newTaskTextNode = document.createTextNode(newTaskName);
-    var taskDateDiv = document.getElementById(NewTaskDate + nowClass);
+    var getNowClass = $(nowClass),
+    		allTask = $("selAll"),
+    		newTaskLi = document.createElement("li"),
+    		newTaskTextNode = document.createTextNode(newTaskName),
+    		taskDateDiv = $(NewTaskDate + nowClass);
     if (taskDateDiv === null) {
         taskDateDiv = document.createElement("div");
         var newDateTextNode = document.createTextNode(NewTaskDate);
@@ -140,16 +140,15 @@ function clickAddTask(event) {//点击添加，提交任务之后，触发的事
 }
 
 function clickCanTask(event) {//点击取消添加任务，触发的事件
-    var newTaskDiv = document.getElementById("newTaskEdit");
+    var newTaskDiv = $("newTaskEdit");
     newTaskDiv.style.display = "none";
 }
 
 function selAllTask(event) {//选择点击"所有任务"的触发事件
-    event = event || window.event;
-    var srcAll = event.srcElement;
-    var tasksEleKid = document.getElementById("tasks").childNodes;
-    var defaultClass = document.getElementById("defaultClass");
-    var childs = srcAll.parentNode.childNodes;
+    var srcAll = getSrcElement(event),
+    		tasksEleKid = $("tasks").childNodes,
+    		defaultClass = $("defaultClass"),
+    		childs = srcAll.parentNode.childNodes;
     defaultClass.style.color = "#000";
     for (var i = 0, length1 = childs.length; i < length1; i++) {
         if (childs[i].nodeType == 1) childs[i].style.color = "#000";
@@ -164,13 +163,12 @@ function selAllTask(event) {//选择点击"所有任务"的触发事件
 }
 
 function selingClass(event) {//选择点击类别事件
-    event = event || window.event;
-    var srcClass = event.srcElement;
+    var srcClass = getSrcElement(event);
     if (srcClass.nodeName == "SPAN") {
         srcClass = srcClass.parentNode;
     }
-    var tasksEleKid = document.getElementById("tasks").childNodes;
-    var selAll = document.getElementById("selAll");
+    var tasksEleKid = $("tasks").childNodes,
+    		selAll = $("selAll");
     var childs = srcClass.parentNode.childNodes;
     for (var i = 0, length1 = childs.length; i < length1; i++) {
         if (childs[i].nodeType == 1) childs[i].style.color = "#000";
@@ -189,38 +187,35 @@ function selingClass(event) {//选择点击类别事件
 }
 
 function hoverClass(event) {//类别列表hover事件
-    event = event || window.event;
-    var srcClass = event.srcElement;
-    var delImg = document.getElementById("delTag" + srcClass.classNum);
+    var srcClass = getSrcElement(event);
+    var delImg = $("delTag" + srcClass.classNum);
     if (delImg !== null) {
         delImg.style.visibility = "visible";
     }
 }
 
 function outClass(event) {//类别列表光标移出触发事件
-    event = event || window.event;
-    var srcClass = event.srcElement;
-    var delImg = document.getElementById("delTag" + srcClass.classNum);
+    var srcClass = getSrcElement(event);
+    var delImg = $("delTag" + srcClass.classNum);
     if (delImg !== null) {
         delImg.style.visibility = "hidden";
     }
 }
 
 function selingTask(event) {//选择点击任务事件
-    event = event || window.event;
-    var srcTask = event.srcElement;
+    var srcTask = getSrcElement(event);
     var operateTask;
     if (srcTask.className != "taskDate" && srcTask.taskName !== undefined) {
         nowTask = srcTask.id;
-        var taskName = document.getElementById("taskName");
-        var taskDate = document.getElementById("taskDate");
-        var taskDetail = document.getElementById("taskDetail");
-        operateTask = document.getElementById(nowTask);
+        var taskName = $("taskName");
+        var taskDate = $("taskDate");
+        var taskDetail = $("taskDetail");
+        operateTask = $(nowTask);
         taskName.innerHTML = srcTask.taskName;
         taskDate.innerHTML = "任务日期：" + srcTask.taskDate;
         taskDetail.value = operateTask.taskDetail;
     }
-    var doneButton = document.getElementById("doneButton");
+    var doneButton = $("doneButton");
     if (operateTask.doneTag === false) {
         doneButton.style.display = "block";
     } else {
@@ -229,8 +224,8 @@ function selingTask(event) {//选择点击任务事件
 }
 
 function doneTask(event) {//点击完成任务图标的触发事件
-    var operateTask = document.getElementById(nowTask);
-    var doneButton = document.getElementById("doneButton");
+    var operateTask = $(nowTask),
+    		doneButton = $("doneButton");
     var sureDone = confirm("确定完成吗？");
     if (sureDone) {
         operateTask.doneTag = true;
@@ -240,18 +235,18 @@ function doneTask(event) {//点击完成任务图标的触发事件
 }
 
 function editTask(event) {//点击编辑任务图标的触发事件
-    var taskDetail = document.getElementById("taskDetail");
+    var taskDetail = $("taskDetail");
     taskDetail.focus();
     taskDetail.onchange = saveDetails;
 }
 
 function saveDetails(event) {//任务详细描述框内容改变后的触发事件，保存内容
-    var operateTask = document.getElementById(nowTask);
+    var operateTask = $(nowTask);
     operateTask.text = taskDetail.value;
 }
 
 function showAll(event) {//选择点击"所有"的触发事件
-    var tasks = document.getElementById("tasks").childNodes;
+    var tasks = $("tasks").childNodes;
     for (var i = 0, length1 = tasks.length; i < length1; i++) {
         if (tasks[i].nodeType == 1) {
             tasks[i].style.display = "block";
@@ -260,7 +255,7 @@ function showAll(event) {//选择点击"所有"的触发事件
 }
 
 function showUndo(event) {//选择点击"未完成"的触发事件
-    var tasks = document.getElementById("tasks").childNodes;
+    var tasks = $("tasks").childNodes;
     for (var i = 0, length1 = tasks.length; i < length1; i++) {
         if (tasks[i].nodeType == 1 && tasks[i].doneTag === true) {
             tasks[i].style.display = "none";
@@ -271,7 +266,7 @@ function showUndo(event) {//选择点击"未完成"的触发事件
 }
 
 function showDone(event) {//选择点击"已完成"的触发事件
-    var tasks = document.getElementById("tasks").childNodes;
+    var tasks = $("tasks").childNodes;
     for (var i = 0, length1 = tasks.length; i < length1; i++) {
         if (tasks[i].nodeType == 1 && tasks[i].doneTag === false) {
             tasks[i].style.display = "none";
@@ -282,16 +277,15 @@ function showDone(event) {//选择点击"已完成"的触发事件
 }
 
 function delClass(event) {//删除类别的触发事件
-    event = event || window.event;
-    var srcImg = event.srcElement;
-    var classLiEle = document.getElementById("newClass" + srcImg.classNum);
+    var srcImg = getSrcElement(event);
+    var classLiEle = $("newClass" + srcImg.classNum);
     if (confirm("确定删除？")) {
-        var tasks = document.getElementById("tasks").childNodes;
+        var tasks = $("tasks").childNodes;
         for (var i = tasks.length - 1; i >= 0; i--) {
             if (tasks[i].nodeType == 1 && tasks[i].belongTo == classLiEle.id) {
                 tasks[i].parentNode.removeChild(tasks[i]);
                 taskCount--;
-                var allTask = document.getElementById("selAll");
+                var allTask = $("selAll");
                 allTask.innerHTML = "所有任务(" + taskCount + ")";
             }
         }
@@ -301,5 +295,14 @@ function delClass(event) {//删除类别的触发事件
 
 function isDate(dat) {//判断日期格式是否为：yy-mm-dd
     return /^(\d{4})-(\d{2})-(\d{2})$/.test(dat);
+}
+
+function $ ( id ) {//封装常用函数
+	return document.getElementById(id);
+}
+
+function getSrcElement (event) {//获取事件源元素
+	    event = event || window.event;
+	    return event.srcElement;
 }
 window.onload = todo;//载入页面后执行主程序
